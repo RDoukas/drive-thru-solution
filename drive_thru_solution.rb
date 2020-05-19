067242d6fd8cf4df49c9eadb45574e286cdde314
Becca <rrdoukas@gmail.com>
2020-05-19 18:45:24 -0500

    refractor drive thru

--- drive_thru_solution.rb
+++ drive_thru_solution.rb
@@ -17,34 +17,45 @@
 
 # Commit 3 - Initial Solution
 
-def drive_thru
-  response_arr = ["Don't look at the menu, look at ME!", "I order for you! DOUBLECHEEZ!", "NO FRIES - CHIPS", "No Pepsi - COKE!"]
-  puts "Welcome to Ruby Burger. We have specials on Hamburgers, Fish Fillet and Cookies for a dollar. Can I take your order?"
-  order = gets.chomp
-  if order == "THATS IT"
-    puts "Pull up to the first window."
-  elsif order == order.upcase
-    puts response_arr.sample
-  else 
-    puts  "I'm sorry, you'll have to speak up Ma'am."
-  end 
-
-end
+# def drive_thru
+#   response_arr = ["Don't look at the menu, look at ME!", "I order for you! DOUBLECHEEZ!", "NO FRIES - CHIPS", "No Pepsi - COKE!"]
+#   puts "Welcome to Ruby Burger. We have specials on Hamburgers, Fish Fillet and Cookies for a dollar. Can I take your order?"
+#   order = gets.chomp
+#   if order != order.upcase
+#   puts  "I'm sorry, you'll have to speak up Ma'am."
+#   elsif order == "THATS IT"
+#     puts "Pull up to the first window."
+#   else 
+#     puts response_arr.sample
+#   end 
+# end 
 
 # Commit 4 - Refactored Solution
 
   # Step 1
-  # vvvvvvvvvvv copy your initial solution here vvvvvvvvvvvvvvvv
+
+  # def drive_thru
+  #     response_arr = ["Don't look at the menu, look at ME!", "I order for you! DOUBLECHEEZ!", "NO FRIES - CHIPS", "No Pepsi - COKE!"]
+  #     puts "Welcome to Ruby Burger. We have specials on Hamburgers, Fish Fillet and Cookies for a dollar. Can I take your order?"
+  #     order = gets.chomp
+  #     if order == "THATS IT"
+  #       puts "Pull up to the first window."
+  #     elsif order == order.upcase
+  #       puts response_arr.sample
+  #     else 
+  #       puts  "I'm sorry, you'll have to speak up Ma'am."
+  #     end 
+    
+  #   end
 
 
-
-  # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
   
   # Step 2: comment out your original code above in "Initial Solution" section
   # Step 3: then refactor in this section.
 
+
 # Runner Code
 
-drive_thru
+# drive_thru
 
 
