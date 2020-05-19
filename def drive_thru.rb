8b646e1cea1c3545d56689e7c6f029f87108d595
Becca <rrdoukas@gmail.com>
2020-05-19 18:08:07 -0500

    initial solution drive thru

--- def drive_thru.rb
+++ def drive_thru.rb
@@ -2,12 +2,12 @@ def drive_thru
   response_arr = ["Don't look at the menu, look at ME!", "I order for you! DOUBLECHEEZ!", "NO FRIES - CHIPS", "No Pepsi - COKE!"]
   puts "Welcome to Ruby Burger. We have specials on Hamburgers, Fish Fillet and Cookies for a dollar. Can I take your order?"
   order = gets.chomp
-  if order == order.downcase
-    puts  "I'm sorry, you'll have to speak up Ma'am."
-  elsif order == "THATS IT"
+  if order == "THATS IT"
     puts "Pull up to the first window."
-  else 
+  elsif order == order.upcase
     puts response_arr.sample
+  else 
+    puts  "I'm sorry, you'll have to speak up Ma'am."
   end 
 
 end
