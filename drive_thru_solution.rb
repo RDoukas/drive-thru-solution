39af47db82bf8c1aaaf51d857f72edf1cc1d8dfe
Becca <rrdoukas@gmail.com>
2020-05-19 19:04:48 -0500

    updates bonus 1

--- drive_thru_solution.rb
+++ drive_thru_solution.rb
@@ -59,3 +59,33 @@
 # drive_thru
 
 
+#Bonus 1
+
+
+  def drive_thru
+      welcome_arr = ["Welcome to Good Burger, home of the Good Burger. Can I take your order?", "Welcome to Burger King. May I please take your order?", "Cheezborger! Cheezborger! You want doublecheez?!?Who’s next!?! WHO’S NEXT!?!"]
+      done_arr = ["Pull forward to the Texaco station.", "Take a right and go five and a half miles southeast", "Talk to the guy in the yellow ponch, his name is Hank.", "Hank will take you to the whopper lair."]
+      louder_arr = ["Can you turn that up for me right now?", "Gotta say it loud, gotta shout it out!", "C'mon people let's get loud"]
+      caps_arr = ["Don't look at the menu, look at ME!", "I order for you! DOUBLECHEEZ!", "NO FRIES - CHIPS", "No Pepsi - COKE!", "Excuse me, I'm bleeding from the ears here okay?"]
+      puts "Welcome to Ruby Burger. We have specials on Hamburgers, Fish Fillet and Cookies for a dollar. Can I take your order?"
+      order = gets.chomp
+      if order == "THATS IT"
+        yell_count = 1
+        while yell_count < 3
+          puts "Say it again:"
+          order = gets.chomp
+          if order == "THATS IT" 
+            yell_count += 1
+          end 
+        end 
+        puts "Pull up to the first window."
+      elsif order == order.upcase
+        puts response_arr.sample
+      else 
+        puts  "I'm sorry, you'll have to speak up Ma'am."
+      end 
+    
+   end
+
+    drive_thru
+
